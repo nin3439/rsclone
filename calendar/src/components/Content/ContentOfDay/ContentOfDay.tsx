69c6ccd282b6c0b0c41cf1f580a77dfc2c894d78
Anton <kubana6@gmail.com>
2021-01-10 01:56:51 +0300

    added formik

--- calendar/src/components/Content/ContentOfDay/ContentOfDay.tsx
+++ calendar/src/components/Content/ContentOfDay/ContentOfDay.tsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React from 'react';
+import { useFormik } from 'formik';
+
+ import React from 'react';
+ import { useFormik } from 'formik';
+
+ export const SignupForm = () => {
+   // Pass the useFormik() hook initial form values and a submit function that will
+   // be called when the form is submitted
+   const formik = useFormik({
+     initialValues: {
+       email: '',
+     },
+     onSubmit: values => {
+       alert(JSON.stringify(values, null, 2));
+     },
+   });
+   return (
+     <form onSubmit={formik.handleSubmit}>
+       <label htmlFor="email">Email Address</label>
+       <input
+         id="email"
+         name="email"
+         type="email"
+         onChange={formik.handleChange}
+         value={formik.values.email}
+       />
+       <button type="submit">Submit</button>
+     </form>
+   );
+ };
 
 // import classes from "./ContentOfDay.module.css";
 
-export const ContentOfDay = () => {
-  return <div>ContentOfDay</div>;
-};
-
-
+// export const ContentOfDay = () => {
+//   return <div>ContentOfDay</div>;
+// };
