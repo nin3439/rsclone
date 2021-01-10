005cb998ce126a6b51a3bfb4bebdeb39b867aa73
Andrei Roh <andrei_roh@tut.by>
2021-01-10 20:23:29 +0300

    feat: add date picker
    Add date information & date picker

--- calendar/src/components/Widgets/Widgets.tsx
+++ calendar/src/components/Widgets/Widgets.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { Sidebar } from './components/Sidebar/Sidebar';
 
 import classes from "./Widgets.module.css";
 
-export const Widgets = () => {
-  return <div className={classes.item}>Widgets</div>;
+export const Widgets = ({ date, changeDate } : {date: any, changeDate: any}) => {
+  return (
+    <div className={classes.item}>
+      <Sidebar date={date} changeDate={changeDate} />
+    </div>
+  );
 };
