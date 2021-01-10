330dc0ab8dfd5500136ea7841fa9a1e9e69d99c5
Andrei Roh <andrei_roh@tut.by>
2021-01-10 20:23:29 +0300

    feat: add date picker
    Add date information & date picker

---
+++ calendar/src/components/Widgets/components/Sidebar/components/Calendar/Calendar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment';
+
+export const Calendar = ({ date, changeDate } : {date: any, changeDate: any}) => {
+    return (
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+            <DatePicker variant="static" value={date} onChange={changeDate} />
+        </MuiPickersUtilsProvider>
+    )
+}
