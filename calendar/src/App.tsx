fe68cbc8997937c5a436666ced0110b46067de4b
Anton <kubana6@gmail.com>
2021-01-14 23:46:07 +0300

    delete r

--- calendar/src/App.tsx
+++ calendar/src/App.tsx
@@ -1,16 +1,7 @@
-
 import React, { useState } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
 
 import { Header } from './components/Header/Header';
 import { Widgets } from './components/Widgets/Widgets';
-import { Content } from './components/Content/Content';
-import { ContentOfDay } from './components/Content/ContentOfDay/ContentOfDay';
-import { ContentOfWeek } from './components/Content/ContentOfWeek/ContentOfWeek';
-import { ContentOfMonth } from './components/Content/ContentOfMonth/ContentOfMonth';
-import { ContentOfYear } from './components/Content/ContentOfYear/ContentOfYear';
-import { ContentOfSchedule } from './components/Content/ContentOfShedule/ContentOfSchedule';
-import { ContentOfFourDay } from './components/Content/ContentOfFourDay/ContentOfFourDay';
 import Schedule from './components/Content/Schedule/Schedule';
 
 import './App.css';
@@ -21,20 +12,11 @@ export const App = () =>  {
   const [active, setValue] = useState(false)
 
   return (
-    // <BrowserRouter>
       <div className="App">
         <Header />
         <Widgets date={date} changeDate={changeDate} />
         <div className="App-content">
           <Schedule active={active} setValue={setValue} />
-          {/* <Route exact path="/" component={} />
-          <Route exact path="/day" component={ContentOfDay} />
-          <Route exact path="/week" component={ContentOfWeek} />
-          <Route exact path="/month" component={ContentOfMonth} />
-          <Route exact path="/year" component={ContentOfYear} />
-          <Route exact path="/schedule" component={ContentOfSchedule} />
-          <Route exact path="/fourDay" component={ContentOfFourDay} /> */}
         </div>
       </div>
-    // </BrowserRouter>
   )}
--- calendar/src/components/Header/Header.tsx
+++ calendar/src/components/Header/Header.tsx
@@ -1,49 +1,11 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 
 import classes from "./Header.module.css";
-const linksData = [
-  { id: 1,
-    path: 'day',
-    name: 'Day',
-  },
-  { id: 2,
-    path: 'week',
-    name: 'Week',
-  },
-  { id: 3,
-    path: 'month',
-    name: 'Month',
-  },
-  { id: 4,
-    path: 'year',
-    name: 'Year',
-  },
-  { id: 5,
-    path: 'schedule',
-    name: 'Schedule',
-  },
-  { id: 6,
-    path: 'fourDay',
-    name: 'Four Day',
-  }
-]
 
-// const links = linksData.map(element => {
-//   return (
-//     <NavLink to={`/${element.path}`} className={classes.item}>
-//       {element.name}
-//     </NavLink>
-//   );
-
-// })
 export const Header = () => {
   return (
     <div className={classes.header}>
       Header
-      <div>
-      </div>
-
     </div>
   );
 };
