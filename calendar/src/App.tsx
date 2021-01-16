489cd321df4312067e2fafd78cbab1b60072d807
nin3439 <ninok33_94@mail.ru>
2021-01-16 21:18:06 +0300

    fix: fix type date

--- calendar/src/App.tsx
+++ calendar/src/App.tsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
-
 import { Header } from './components/Header/Header';
 import { Widgets } from './components/Widgets/Widgets';
 import { EventsSchedule } from './components/Content/Schedule/Schedule';
 import { useTranslation } from 'react-i18next';
+import moment, {  Moment } from 'moment'
 import './App.css';
 
 export const App = () => {
-  const [date, changeDate] = useState(new Date());
   const { t, i18n } = useTranslation();
   const [active, setActive] = useState(true);
+  const [date, changeDate] = useState<Moment | null>(moment());
+ 
   return (
     <div className="App">
       {t('create')}
